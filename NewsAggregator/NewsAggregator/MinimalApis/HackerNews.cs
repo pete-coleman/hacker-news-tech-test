0c3f74552efa692634351a610ccea834a72785bd
Pete Coleman <pete@petecolemansoftware.co.uk>
2024-02-16 14:26:59 +0000

    Add basic structure for minimal api and .http file

---
+++ NewsAggregator/NewsAggregator/MinimalApis/HackerNews.cs
@@ -0,0 +1,18 @@
+﻿namespace NewsAggregator.MinimalApis
+{
+    public static class HackerNews
+    {
+        public static void Bind(WebApplication app)
+        {
+            app.MapGet($"api/hackernews/stories/best", (int count) =>
+            {
+                if (count <= 0) {
+                    return Results.BadRequest();
+                }
+                return Results.Ok();
+            })
+            .WithName("GetBestNStories")
+            .WithOpenApi();
+        }
+    }
+}
